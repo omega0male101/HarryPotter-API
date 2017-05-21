d884ca7415185fc2ff98bac92b9c7330c934f0ff
Eden <edenstevens101@gmail.com>
2017-05-21 23:37:03 +0100

    Done a little CSS and fixed issues.

--- public/displayCharacters.js
+++ public/displayCharacters.js
@@ -2,12 +2,11 @@ var DisplayCharacters = function(container) {
   this.container = container
 }
 
-
 DisplayCharacters.prototype = {
 
   Page: function(character) {
     var pTags = this.container.querySelectorAll('p') 
-    pTags[0].innerText = character.name + " is reconised whith their efforts in the house of " + character.house + ". Be warey as the generally carry a " + character.wand.core + " wand."
+    pTags[0].innerText = character.name + " is reconised whith their efforts in the house of " + character.house + ". Be warey as the generally carry a " + character.wand.core + " wand!"
   }
 
 }
