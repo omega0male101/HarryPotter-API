c61cba0d68b5a9c7b50611c835ef42551378c95b
Eden <edenstevens101@gmail.com>
2017-05-21 23:54:59 +0100

    Added image but does not work.

--- public/displayCharacters.js
+++ public/displayCharacters.js
@@ -5,8 +5,12 @@ var DisplayCharacters = function(container) {
 DisplayCharacters.prototype = {
 
   Page: function(character) {
-    var pTags = this.container.querySelectorAll('p') 
+    var picture = this.container.querySelectorAll('img')
+    var pTags = this.container.querySelectorAll('p')  
     pTags[0].innerText = character.name + " is reconised whith their efforts in the house of " + character.house + ". Be warey as the generally carry a " + character.wand.core + " wand!"
+    /Coult not get this to load picture
+    picture = character.image
+    img.style.backgroundImage = "url(" + picture + ")"
   }
 
 }
