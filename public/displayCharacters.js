4f0902ff44ed7956002ca929b1a1c6c30a651e6e
Eden <edenstevens101@gmail.com>
2017-05-21 23:26:34 +0100

    Added info text with details

--- public/displayCharacters.js
+++ public/displayCharacters.js
@@ -8,7 +8,6 @@ DisplayCharacters.prototype = {
   Page: function(character) {
     var pTags = this.container.querySelectorAll('p') 
     pTags[0].innerText = character.name + " is reconised whith their efforts in the house of " + character.house + ". Be warey as the generally carry a " + character.wand.core + " wand."
-   
   }
 
 }
